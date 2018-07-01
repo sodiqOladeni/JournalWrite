f8eb0f166ff9f983e05d3b8a4b48b75b9ce51b9f
sodiqOladeni <sodiqoladeni@gmail.com>
2018-07-01 21:45:23 +0100

    apk and screen shot design file added

--- app/src/main/java/android/africanego/com/journalwrite/JournalListAdapter.java
+++
@@ -1,7 +0,0 @@
-package android.africanego.com.journalwrite;
-
-/**
- * Created by ${sodiqOladeni} on 6/29/2018.
- */
-public class JournalListAdapter {
-}
--- app/src/main/java/android/africanego/com/journalwrite/database/Entity.java
+++
@@ -1,7 +0,0 @@
-package android.africanego.com.journalwrite.database;
-
-/**
- * Created by ${sodiqOladeni} on 6/29/2018.
- */
-public class Entity {
-}
